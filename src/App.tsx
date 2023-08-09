c099d880e4778e0b79c71add5ca2c4468cccd616
smkim <ksm0057@naver.com>
2023-08-09 22:32:54 +0900

    redux Provide + UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 
 type OwnProps = {
@@ -11,11 +12,24 @@ type OwnProps = {
 };
 
 function App(props: OwnProps) {
+  const [todoValue, setTodoValue] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    //새로고침 방지
+    e.preventDefault();
+    setTodoValue("");
+  };
   return (
     <div className="App">
       Clicked: {props.value.counter} times
       <button onClick={props.onIncrement}>+</button>
       <button onClick={props.onDecrement}>-</button>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
